c8024637e343d01a2ffc557280b510ba3dffa262
muruganu <40798112+muruganu@users.noreply.github.com>
2022-06-20 16:20:42 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_budgets_budget" "allservices-monthly" {
   name              = "allservices-monthly"
   budget_type       = "COST"
-  limit_amount      = "15"
+  limit_amount      = "10"
   limit_unit        = "USD"
   time_unit         = "MONTHLY"
 
